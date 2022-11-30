994d3df0bc04a5dac3662d15443533de7aa6ae2c
AjinkyaVir <avirkud7@gmail.com>
2022-11-30 14:23:51 +0530

    TestNG @Test annotation

---
+++ src/test/java/com/testNG_Practice/test/Module1.java
@@ -0,0 +1,14 @@
+package com.testNG_Practice.test;
+
+import org.testng.annotations.Test;
+
+public class Module1 {
+
+	@Test
+	public void m1() {
+		
+		System.out.println("Module 1 - M1() method executed");
+		
+	}
+	
+}
---
+++ src/test/java/com/testNG_Practice/test/Module2.java
@@ -0,0 +1,17 @@
+package com.testNG_Practice.test;
+
+import org.testng.annotations.Test;
+
+public class Module2 {
+
+	@Test
+	public void m1() {
+		System.out.println("Module 2 - m1() method executed");
+	}
+	
+	@Test
+	public void m2() {
+		System.out.println("Module 2 - m2() method executed");
+	}
+	
+}
