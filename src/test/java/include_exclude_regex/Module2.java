c4133fac2187480478287f4898cc68822f8986b5
AjinkyaVir <avirkud7@gmail.com>
2022-11-30 14:58:47 +0530

    include/exclude test case by Running test cases with Regex

---
+++ src/test/java/include_exclude_regex/Module2.java
@@ -0,0 +1,29 @@
+package include_exclude_regex;
+
+import org.testng.annotations.Test;
+
+public class Module2 {
+
+	
+
+	@Test
+	public void webCarLoanLogin() {
+		System.out.println("webCarLoanLogin() executed");
+	}
+	
+	@Test
+	public void webHomeLoanLogin() {
+		System.out.println("webHomeLoanLogin()executed");
+	}
+	
+	@Test
+	public void mobileCarLoanLogin() {
+		System.out.println("mobileCarLoanLogin()executed");
+	}
+	
+	@Test
+	public void mobileHomeLoanLogin() {
+		System.out.println("mobileHomeLoanLogin()executed");
+	}
+	
+}
